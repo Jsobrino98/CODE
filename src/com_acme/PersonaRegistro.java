168f90b48fbf92f11bba0ebf513eef00a3cd5d0d
jorge <jsobrino98@gmail.com>
2024-02-26 09:29:40 +0100

    He comentado la linea que considero que sobra

--- src/com_acme/PersonaRegistro.java
+++ src/com_acme/PersonaRegistro.java
@@ -3,7 +3,8 @@ package com_acme;
 public record PersonaRegistro(String nombre, String apellidos) {
     @Override
     public String toString() {
-        System.out.println("Esta instruccion es invisible");
+        //Comento la linea de codigo que considero que se puede eliminar
+        //System.out.println("Esta instruccion es invisible");
         return "%s %s".formatted(nombre, apellidos);
     }
 }
