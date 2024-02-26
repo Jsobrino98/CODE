ceab26ebdee8bf0da0059b32b06a019ea3112817
jorge <jsobrino98@gmail.com>
2024-02-26 09:21:41 +0100

    Se agrego una linea de codigo invisible

--- src/com_acme/PersonaRegistro.java
+++ src/com_acme/PersonaRegistro.java
@@ -3,6 +3,7 @@ package com_acme;
 public record PersonaRegistro(String nombre, String apellidos) {
     @Override
     public String toString() {
+        System.out.println("Esta instruccion es invisible");
         return "%s %s".formatted(nombre, apellidos);
     }
 }
