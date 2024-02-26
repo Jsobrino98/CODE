982f6ac5d5303b996f69459fcb493a596a268a8d
jorge <jsobrino98@gmail.com>
2024-02-26 09:02:46 +0100

    Se integró la clase Mensaje

---
+++ src/com_acme/Mensaje.java
@@ -0,0 +1,16 @@
+package com_acme;
+
+import java.util.Scanner;
+
+public class Mensaje {
+
+    public String solicitarDatos(String mensaje) {
+        System.out.println(mensaje);
+        Scanner teclado = new Scanner(System.in);
+        return teclado.nextLine();
+    }
+
+    public void saludar(String nombre) {
+        System.out.println("Hola %s, benvido a Java \n" + nombre);
+    }
+}
